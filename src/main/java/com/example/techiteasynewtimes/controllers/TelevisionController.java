45dc31e986d68dffe3ac9244c4ef351644071dd7
johnvdpk <johnvdpk@gmail.com>
2023-09-19 21:47:54 +0200

    opnieuw alles schrijven helpt begrijpen

--- src/main/java/com/example/techiteasynewtimes/controllers/TelevisionController.java
+++ src/main/java/com/example/techiteasynewtimes/controllers/TelevisionController.java
@@ -1,6 +1,12 @@
 package com.example.techiteasynewtimes.controllers;
 
+import com.example.techiteasynewtimes.dtos.TelevisionDto;
+import com.example.techiteasynewtimes.models.Television;
 import com.example.techiteasynewtimes.service.TelevisionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +21,11 @@ public class TelevisionController {
 
     }
 
+    @PostMapping
+    public ResponseEntity<TelevisionDto> createTelevision (@RequestBody TelevisionDto televisionDto) {
+        Television television = new Television();
 
+        return new ResponseEntity<>(televisionDto, HttpStatus.CREATED);
+
+    }
 }
--- src/main/java/com/example/techiteasynewtimes/service/TelevisionService.java
+++ src/main/java/com/example/techiteasynewtimes/service/TelevisionService.java
@@ -1,5 +1,7 @@
 package com.example.techiteasynewtimes.service;
 
+import com.example.techiteasynewtimes.dtos.TelevisionDto;
+import com.example.techiteasynewtimes.models.Television;
 import com.example.techiteasynewtimes.repository.TelevisionRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,33 @@ public class TelevisionService {
 
     public TelevisionService (TelevisionRepository televisionRepos) {
         this.televisionRepos = televisionRepos;
+    }
+
+    public TelevisionDto createTelevision(TelevisionDto televisionDto) {
+        Television television = new Television();
+
+        television.setType(televisionDto.type);
+        television.setBrand(televisionDto.brand);
+        television.setName(televisionDto.name);
+        television.setPrice(televisionDto.price);
+        television.setAvailableSize(televisionDto.availableSize);
+        television.setRefreshRate(televisionDto.refreshRate);
+        television.setScreenType(televisionDto.screenType);
+        television.setScreenQuality(televisionDto.screenQuality);
+        television.setSmartTv(televisionDto.smartTv);
+        television.setWifi(televisionDto.wifi);
+        television.setVoiceControl(televisionDto.voiceControl);
+        television.setHdr(televisionDto.hdr);
+        television.setBluetooth(televisionDto.bluetooth);
+        television.setAmbiLight(televisionDto.ambiLight);
+        television.setOriginalStock(televisionDto.originalStock);
+        television.setSold(televisionDto.sold);
+        televisionRepos.save(television);
+
+        return televisionDto;
+
+
+
     }
 
 }
